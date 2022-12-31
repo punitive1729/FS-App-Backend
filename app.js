2ad6ad56386bbc7b4d92a104e558f361f54daf83
Punit Kumar Ojha <45972546+punitive1729@users.noreply.github.com>
2023-01-01 00:46:42 +0530

    Update app.js

--- app.js
+++ app.js
@@ -1,11 +1,13 @@
 const express = require('express');
 const app = express();
 const AppError = require('./utils/AppError');
+const bodyParser = require('body-parser');
 const globalErrorHandler = require('./controller/errorHandler');
 const cors = require('cors');
 const fileRouter = require('./routes/fileRoutes');
 
 app.use(cors());
+app.use(bodyParser.json());
 app.use('/api/v1/files', fileRouter);
 app.all('*', (req, res, next) => {
   next(new AppError(404, `Cannot find ${req.originalUrl} on this server!`));
